6eeb75c212808b7bc7b8d49d2652d7a1db17396b
100082395 <firewizard58fox@gmail.com>
2019-09-10 15:35:15 -0500

    Created an emoji from 4 Turtle Graphics.

---
+++ Emoji.py
@@ -0,0 +1,12 @@
+import turtle
+import square
+
+no = turtle.Turtle()
+ko = turtle.Turtle()
+se = turtle.Turtle()
+que = turtle.Turtle()
+
+def circle(side):
+    for i in range(10):
+        no.forward(25)
+        no.right(30)
